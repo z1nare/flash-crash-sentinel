357a23bd0ec456da394560b4f24d0f7445113ad1
blazinglygray <garbararsen@gmail.com>
2026-01-11 00:17:04 +0000

    Fix mutmut sandbox imports: make services a package and copy it into sandbox

---
+++ python_components/services/__init__.py
@@ -0,0 +1,7 @@
+"""RiskBeacon services package.
+
+This file exists to make `services.*` imports work reliably in tools that copy code
+into a sandbox (e.g., mutation testing) where implicit namespace packages can break.
+"""
+
+
